324ce20b496c8468c005267a4b2efff8475e08d1
Chungwin <37634612+Chungwin@users.noreply.github.com>
2018-06-22 22:55:07 +0200

    Just do AJAX call when click the button
    Referreing to the button and when "click" then execute whats in the function.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
-//XMLHttpRequest is a browser built in tool to download JSON
-var ourRequest = new XMLHttpRequest();
-//Receiving (GET) JSON Data from URL
-ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+// Referreing to the button and when "click" then execute whats in the function
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function () {
 
-//Once data is loaded ...
-ourRequest.onload = function() {
-    // .. save it all in var ourData as JSON format
-    var ourData = JSON.parse(ourRequest.responseText);
-    // .. access the second object.
-    console.log(ourData[1]);
-};
-ourRequest.send();
+    //XMLHttpRequest is a browser built in tool to download JSON
+    var ourRequest = new XMLHttpRequest();
+    //Receiving (GET) JSON Data from URL
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
 
+    //Once data is loaded ...
+    ourRequest.onload = function() {
+        // .. save it all in var ourData as JSON format
+        var ourData = JSON.parse(ourRequest.responseText);
+        // .. access the second object.
+        console.log(ourData[0]);
+    };
+    ourRequest.send();
 
-// .. we did it! From here on, just generic JS tasks!
-// Currently our AJAX Call runs as soon as the page loads
-// But lets juts make it run, when we click on the blue button
+});
