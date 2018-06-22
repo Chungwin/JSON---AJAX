09c95782fa934d2a1c1764b139f1ca4b4130c236
Chungwin <37634612+Chungwin@users.noreply.github.com>
2018-06-22 22:41:43 +0200

    AJAX Call when the page loads
    Currently our AJAX Call runs as soon as the page loads. But lets juts make it run, when we click on the blue button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-//Receiving (GET) JSON Data from URL
 //XMLHttpRequest is a browser built in tool to download JSON
 var ourRequest = new XMLHttpRequest();
+//Receiving (GET) JSON Data from URL
 ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
 
 //Once data is loaded ...
@@ -14,3 +14,5 @@ ourRequest.send();
 
 
 // .. we did it! From here on, just generic JS tasks!
+// Currently our AJAX Call runs as soon as the page loads
+// But lets juts make it run, when we click on the blue button
