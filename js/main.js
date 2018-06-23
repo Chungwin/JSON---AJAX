2011ab161e63f2ad8daeb739d9ea0b206c46621b
Chungwin <37634612+Chungwin@users.noreply.github.com>
2018-06-23 11:15:00 +0200

    When clicking the button, putting out the JSON data in HTML on the page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-// Referreing to the button and when "click" then execute whats in the function
+var animalContainer = document.getElementById("animal-info");
+
+// Referreing to the button "btn" and when "click" then execute whats in the function
 var btn = document.getElementById("btn");
 btn.addEventListener("click", function () {
 
@@ -6,14 +8,24 @@ btn.addEventListener("click", function () {
     var ourRequest = new XMLHttpRequest();
     //Receiving (GET) JSON Data from URL
     ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
-
     //Once data is loaded ...
     ourRequest.onload = function() {
         // .. save it all in var ourData as JSON format
         var ourData = JSON.parse(ourRequest.responseText);
-        // .. access the second object.
-        console.log(ourData[0]);
+        renderHTML(ourData);
     };
     ourRequest.send();
 
 });
+
+// Job of this function is to add HTML to page (id="animal-info")
+function renderHTML(data) {
+    var htmlString = ""
+    for (i = 0; i < data.length; i++) {
+        htmlString += "<p>" + data[i].name + " is a " + data[i].species + ".</p>";
+    }
+
+    //animalContainer declared on top already 
+    animalContainer.insertAdjacentHTML('beforeend', htmlString);
+
+}
