8d688b8da458abbc88934e9c2c1365559d1e1d50
Chungwin <37634612+Chungwin@users.noreply.github.com>
2018-06-23 11:37:23 +0200

    For each click, AJAX call from different URL
    *Added CSS
    *Added a for-loop so that for each click, different iformations from different page URLs will load. Beacause there are not more than three different page URLs, the button will hide after 3 clicks.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var pageCounter = 1;
+
 var animalContainer = document.getElementById("animal-info");
 
 // Referreing to the button "btn" and when "click" then execute whats in the function
@@ -7,7 +9,7 @@ btn.addEventListener("click", function () {
     //XMLHttpRequest is a browser built in tool to download JSON
     var ourRequest = new XMLHttpRequest();
     //Receiving (GET) JSON Data from URL
-    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
     //Once data is loaded ...
     ourRequest.onload = function() {
         // .. save it all in var ourData as JSON format
@@ -15,6 +17,14 @@ btn.addEventListener("click", function () {
         renderHTML(ourData);
     };
     ourRequest.send();
+    pageCounter++
+    //When btn gets clicked first time, URL page 1 loads, page counter increases to 2
+    //When btn gets clicked second time, URL page 2 loads, page counter increases to 3
+    //When btn gets clicked third time, URL page 3 loads, page counter increases to 4
+    //If pageCounter > 3, css class .hide-me will execute
+    if (pageCounter > 3) {
+      btn.classList.add("hide-me");
+    }
 
 });
 
@@ -25,7 +35,7 @@ function renderHTML(data) {
         htmlString += "<p>" + data[i].name + " is a " + data[i].species + ".</p>";
     }
 
-    //animalContainer declared on top already 
+    //animalContainer declared on top already
     animalContainer.insertAdjacentHTML('beforeend', htmlString);
 
 }
